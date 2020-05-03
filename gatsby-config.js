956f6216749c3288ef66752c4dc0e3715232fca7
Narin Sundarabhaya <nrs710@gmail.com>
2020-05-02 21:22:54 -0700

    ➕ : emotion, axios, date-fns, twitch-embed

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,12 @@
+const activeEnv =
+    process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || 'development'
+
+console.log(`Using environment config: '${activeEnv}'`)
+
+require('dotenv').config({
+    path: `.env.${activeEnv}`,
+})
+
 module.exports = {
     siteMetadata: {
         title: `The Music Please.`,
@@ -24,6 +33,7 @@ module.exports = {
                 background_color: `#000000`,
             },
         }
-        // 'gatsby-plugin-offline',
+        'gatsby-plugin-offline',
+        `gatsby-plugin-emotion`
     ],
 }
