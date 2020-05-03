163c439966fe51698463ba68b955b995bdcdb3bc
Narin Sundarabhaya <nrs710@gmail.com>
2020-05-02 21:38:40 -0700

    💚 (config): missing comma

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 const activeEnv =
-    process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || 'development'
+    process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development"
 
 console.log(`Using environment config: '${activeEnv}'`)
 
-require('dotenv').config({
+require("dotenv").config({
     path: `.env.${activeEnv}`,
 })
 
@@ -11,7 +11,7 @@ module.exports = {
     siteMetadata: {
         title: `The Music Please.`,
         description: `The Music Please is a collective of DJ's and Artists`,
-        author: `Ricopella`
+        author: `Ricopella`,
     },
     plugins: [
         `gatsby-plugin-react-helmet`,
@@ -32,8 +32,7 @@ module.exports = {
                 start_url: `/`,
                 background_color: `#000000`,
             },
-        }
-        // 'gatsby-plugin-offline',
-        `gatsby-plugin-emotion`
+        },
+        `gatsby-plugin-emotion`,
     ],
 }
