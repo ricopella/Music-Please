0e282f85890946b2beb6f604cdf7caa232ccfa37
Narin Sundarabhaya <nrs710@gmail.com>
2020-05-08 21:34:14 -0700

    🐛 (Live): add layout for default styling

--- src/pages/live.js
+++ src/pages/live.js
@@ -5,33 +5,40 @@ import IframeResizer from "iframe-resizer-react"
 import Image from "../components/image"
 import Venmo from "../assets/venmo-icon.svg"
 import CashApp from "../assets/cash-app.svg"
+import Layout from "../components/layout"
 
 const Twitch = () => {
   return (
-    <Styled.TwitchContainer>
-      <Styled.TwitchPageContainer>
-        <Styled.LogoWrapper>
-          <Image filename="PLEASE.png" alt="Music Please Main Logo" />
-        </Styled.LogoWrapper>
-        <Styled.TwitchVideosWrapper>
-          <IframeResizer
-            log
-            src="https://player.twitch.tv/?channel=themusicplease&parent=https://themusicplease.com"
-            style={{
-              width: 1,
-              minWidth: "100%",
-              height: 1,
-              minHeight: "100%",
-              border: "none",
-            }}
-          />
-        </Styled.TwitchVideosWrapper>
-        <Styled.LinksWrapper>
-          <img src={Venmo} alt="Venmo" style={{ height: 24, width: 24 }} />
-          <img src={CashApp} alt="CashApp" style={{ height: 24, width: 24 }} />
-        </Styled.LinksWrapper>
-      </Styled.TwitchPageContainer>
-    </Styled.TwitchContainer>
+    <Layout>
+      <Styled.TwitchContainer>
+        <Styled.TwitchPageContainer>
+          <Styled.LogoWrapper>
+            <Image filename="PLEASE.png" alt="Music Please Main Logo" />
+          </Styled.LogoWrapper>
+          <Styled.TwitchVideosWrapper>
+            <IframeResizer
+              log
+              src="https://player.twitch.tv/?channel=themusicplease&parent=https://themusicplease.com"
+              style={{
+                width: 1,
+                minWidth: "100%",
+                height: 1,
+                minHeight: "100%",
+                border: "none",
+              }}
+            />
+          </Styled.TwitchVideosWrapper>
+          <Styled.LinksWrapper>
+            <img src={Venmo} alt="Venmo" style={{ height: 24, width: 24 }} />
+            <img
+              src={CashApp}
+              alt="CashApp"
+              style={{ height: 24, width: 24 }}
+            />
+          </Styled.LinksWrapper>
+        </Styled.TwitchPageContainer>
+      </Styled.TwitchContainer>
+    </Layout>
   )
 }
 
