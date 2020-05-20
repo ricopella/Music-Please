399177bd60403e35643ffa55c5b155409ae78e9a
Narin Sundarabhaya <nrs710@gmail.com>
2020-05-19 22:00:37 -0700

    ✨ (Shop): add hook to get all products

---
+++ src/pages/shop.js
@@ -0,0 +1,53 @@
+import React from "react"
+import useShopifyProducts from "../hooks/useShopifyProducts"
+import styled from "@emotion/styled"
+import SEO from "../components/seo"
+import Footer from "../components/footer"
+
+const ShopContainer = styled.div`
+  height: 100vh;
+  width: 100%;
+  padding: 2rem 2rem;
+  display: grid;
+  grid-template-rows: max-content 1fr max-content;
+`
+
+const ShopProductsWrapper = styled.div`
+  display: grid;
+  grid-template-columns: auto;
+  gap: 1rem;
+`
+
+const ProductWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: repeat(4, max-content);
+`
+
+const Shop = () => {
+  const products = useShopifyProducts()
+
+  return (
+    <div>
+      <SEO title="THE MUSIC PLEASE SHOP" />
+      <ShopContainer>
+        <h1>THE MUSIC PLEASE SHOP</h1>
+        <ShopProductsWrapper>
+          {products.map(item => (
+            <ProductWrapper key={`shop_item_${item.node.shopifyId}`}>
+              <div>{item.node.title.toUpperCase()}</div>
+              <div>{item.node.description.toUpperCase()}</div>
+              <div>{`$${item.node.priceRange.maxVariantPrice.amount}`}</div>
+              <div>
+                {item.node.availableForSale ? `IN STOCK` : `OUT OF STOCK`}
+              </div>
+            </ProductWrapper>
+          ))}
+        </ShopProductsWrapper>
+        <Footer />
+      </ShopContainer>
+    </div>
+  )
+}
+
+export default Shop
