ef2da46028c77e458cdd516eb5aaeebafbb8eb86
Narin Sundarabhaya <nrs710@gmail.com>
2020-05-19 21:00:49 -0700

    ♻️ (Index): Remove height listener

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,6 @@ import { Link } from "gatsby"
 import styled from "@emotion/styled"
 import { OutboundLink } from "gatsby-plugin-google-analytics"
 import Footer from "../components/footer"
-import { useWindowSize } from "react-use"
 import Bouncy from "../images/gif/BOUNCY.gif"
 
 const LiveLink = styled(Link)`
@@ -19,8 +18,7 @@ const PageContainer = styled.div`
   display: grid;
   grid-template-columns: 1fr;
   grid-template-rows: 1fr max-content;
-  height: 100vh; /* Fallback for browsers that do not support Custom Properties */
-  height: ${props => props.vh}px;
+  height: 100vh;
   width: 100vw;
   background-color: black;
   background-image: url(${Bouncy});
@@ -28,10 +26,8 @@ const PageContainer = styled.div`
 `
 
 const IndexPage = () => {
-  const { height } = useWindowSize()
-
   return (
-    <PageContainer vh={height}>
+    <PageContainer>
       <SEO title="THE MUSIC PLEASE ONLINE" />
       <Layout>
         <div className="wrapper">
---
+++ src/styles/constants.js
@@ -0,0 +1,5 @@
+export const mediaBreakpoints = {
+    phone: `25.875rem`,
+    smallDesktop: `68.75rem`,
+    tablet: `48rem`,
+}
