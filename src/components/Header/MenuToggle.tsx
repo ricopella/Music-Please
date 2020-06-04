d11b95f4c5630e6f5f8dd70704409ca0adc997eb
Narin Sundarabhaya <nrs710@gmail.com>
2020-06-03 22:24:40 -0700

    ♻️ (Header): hamber to chevron

--- src/components/Header/MenuToggle.tsx
+++ src/components/Header/MenuToggle.tsx
@@ -1,47 +1,36 @@
+import HeaderStyled from './Header.styles'
 import React, { FC } from 'react'
-import Styled from './Header.styles'
+import styled from '../../styled'
 import { BUTTON_ANIMATION } from '../../styles/constants'
-import { Cycle } from 'framer-motion'
+import { Cycle, motion } from 'framer-motion'
 
-const MENU_WIDTH = 23
-const MENU_HEIGHT = 23
-
-const Path = props => <Styled.Path {...props} />
+const Chevron = styled(motion.div)`
+  border-right: 0.2em solid ${props => props.theme.colors.action};
+  border-top: 0.2em solid ${props => props.theme.colors.action};
+  height: 1.25rem;
+  position: relative;
+  width: 1.25rem;
+`
 
 const MenuToggle: FC<{ toggle: Cycle }> = ({ toggle }) => (
-  <Styled.MenuToggleButton
+  <HeaderStyled.MenuToggleButton
     onClick={toggle}
     {...BUTTON_ANIMATION}
     className="hamburger_menu"
     aria-label="navigation button"
   >
-    <svg
-      width={MENU_WIDTH}
-      height={MENU_WIDTH}
-      viewBox={`0 0 ${MENU_WIDTH} ${MENU_HEIGHT}`}
-    >
-      <Path
-        variants={{
-          closed: { d: "M 2 2.5 L 20 2.5" },
-          open: { d: "M 3 16.5 L 17 2.5" },
-        }}
-      />
-      <Path
-        d="M 2 9.423 L 20 9.423"
-        variants={{
-          closed: { opacity: 1 },
-          open: { opacity: 0 },
-        }}
-        transition={{ duration: 0.1 }}
-      />
-      <Path
-        variants={{
-          closed: { d: "M 2 16.346 L 20 16.346" },
-          open: { d: "M 3 2.5 L 17 16.346" },
-        }}
-      />
-    </svg>
-  </Styled.MenuToggleButton>
+    <Chevron
+      variants={{
+        open: {
+          rotate: 135,
+        },
+        closed: {
+          rotate: 315,
+        },
+      }}
+      transition={{ duration: 0.5 }}
+    />
+  </HeaderStyled.MenuToggleButton>
 )
 
 export default MenuToggle
