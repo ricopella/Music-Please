b1a0b2bfed698559cf44b65989d42e2b6afeb317
Narin Sundarabhaya <narins@narins-mbp-2020.home>
2020-07-05 21:47:27 -0700

    🐛 (Header): shop external

--- src/components/Header/config.ts
+++ src/components/Header/config.ts
@@ -3,7 +3,7 @@ import ROUTES from "../../config/routes"
 const NAV_ITEMS = [
   { path: ROUTES.root, title: `home` },
   // { path: ROUTES.about, title: "about" },
-  { path: ROUTES.shop, title: `shop` },
+  { path: ROUTES.shop, title: `shop`, external: true },
   { path: ROUTES.live, title: "live" },
   { path: ROUTES.sounds, title: "sounds" },
   { path: ROUTES.contact, title: `contact` },
