a960754338abc26919678741ccb71750202eaadc
Narin Sundarabhaya <nrs710@gmail.com>
2022-05-21 01:47:02 +0700

    ✨ daywine

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,37 +1,22 @@
 const path = require(`path`)
 
 exports.createPages = ({ graphql, actions }) => {
-    const { createPage, createRedirect } = actions
+  const { createPage, createRedirect } = actions
 
-    // REDIRECT FOR /SHOP
-    createRedirect({
-        fromPath: "/shop",
-        toPath: "https://musicpleaseshop.myshopify.com/collections/all",
-        isPermanent: true,
-        force: true
-    });
-
-    //   return graphql(`
-    //   {
-    //     allShopifyProduct {
-    //       edges {
-    //         node {
-    //           handle
-    //         }
-    //       }
-    //     }
-    //   }
-    // `).then(result => {
-    //       result.data.allShopifyProduct.edges.forEach(({ node }) => {
-    //           createPage({
-    //               path: `/shop/${node.handle}/`,
-    //               component: path.resolve(`./src/templates/ProductPage/index.tsx`),
-    //               context: {
-    //                   // Data passed to context is available
-    //                   // in page queries as GraphQL variables.
-    //                   handle: node.handle,
-    //               },
-    //           })
-    //       })
-    //   })
+  // REDIRECT FOR /SHOP
+  createRedirect(
+    {
+      fromPath: "/shop",
+      toPath: "https://musicpleaseshop.myshopify.com/collections/all",
+      isPermanent: true,
+      force: true,
+    },
+    {
+      toPath:
+        "https://www.eventbrite.com/e/daywine-tropical-dance-party-tickets-345566487667",
+      fromPath: "/daywine",
+      isPermanent: true,
+      force: true,
+    }
+  )
 }
