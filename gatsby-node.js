0e3d1b1178be662a49eb7d9e15723feda3f76cbc
Narin Sundarabhaya <nrs710@gmail.com>
2022-05-21 02:03:14 +0700

    ✏️  multiple redirects

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,19 +4,18 @@ exports.createPages = ({ graphql, actions }) => {
   const { createPage, createRedirect } = actions
 
   // REDIRECT FOR /SHOP
-  createRedirect([
-    {
-      fromPath: "/shop",
-      toPath: "https://musicpleaseshop.myshopify.com/collections/all",
-      isPermanent: true,
-      force: true,
-    },
-    {
-      fromPath: "/daywine",
-      toPath:
-        "https://www.eventbrite.com/e/daywine-tropical-dance-party-tickets-345566487667",
-      isPermanent: true,
-      force: true,
-    },
-  ])
+  createRedirect({
+    fromPath: "/shop",
+    toPath: "https://musicpleaseshop.myshopify.com/collections/all",
+    isPermanent: true,
+    force: true,
+  })
+
+  createRedirect({
+    fromPath: "/daywine",
+    toPath:
+      "https://www.eventbrite.com/e/daywine-tropical-dance-party-tickets-345566487667",
+    isPermanent: true,
+    force: true,
+  })
 }
