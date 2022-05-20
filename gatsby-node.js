12196aec8d4b00617ed99609d70c8d1517bca055
Narin Sundarabhaya <nrs710@gmail.com>
2022-05-21 02:00:12 +0700

    ✏️ make redirect array

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,7 +4,7 @@ exports.createPages = ({ graphql, actions }) => {
   const { createPage, createRedirect } = actions
 
   // REDIRECT FOR /SHOP
-  createRedirect(
+  createRedirect([
     {
       fromPath: "/shop",
       toPath: "https://musicpleaseshop.myshopify.com/collections/all",
@@ -12,11 +12,11 @@ exports.createPages = ({ graphql, actions }) => {
       force: true,
     },
     {
+      fromPath: "/daywine",
       toPath:
         "https://www.eventbrite.com/e/daywine-tropical-dance-party-tickets-345566487667",
-      fromPath: "/daywine",
       isPermanent: true,
       force: true,
-    }
-  )
+    },
+  ])
 }
